250ae192786012a9e7d598fb646986b574afe996
wascoyur <mytem20007@gmail.com>
2020-10-18 09:19:14 +0500

    fix: my implement begin

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
   let out = false;
-  if (str % 2 !== 0) {
+  let stack = [];
+  if (str % 2 !== 0 || str === '') {
     return out;
   }
   let config = Array.from(bracketsConfig);
-  for (let index = 0; index < config.length; index++) {
-    const element = array[index];
+  for (let index = 0; index < str.length; index++) {
+
 
   }
+  function iterating(one_bracket) {
+    bracketsConfig.forEach(item => {
+      
+      item[0] === one_bracket
+    })
+    let id_bracket = [];
 
-
+    return
+  }
   return out;
 }
